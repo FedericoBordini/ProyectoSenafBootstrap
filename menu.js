bb45f72e196f37a8fbbd0bb70ac5f8ba557c9f80
Federico <fedetrabajo13@gmail.com>
2020-11-26 11:23:54 -0300

    mover iconos

--- menu.js
+++
@@ -1,6 +0,0 @@
-/* Referido a sub menu tengo ideas */
-const ideas = document.getElementById('menu-ideas');
-/* Referido a icono de telefono tengo ideas */
-const tengo = document.getElementById('tengo-ideas');
-
-ideas.addEventListener('click', () =>tengo.classList.toggle('menu--show'));
